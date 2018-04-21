2968f59378d1833109d56229e2d3703d6aab97de
NikJoj <nikhiljoji07@gmail.com>
2018-04-21 13:34:15 +0530

    UI changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 3000
 
 app.get('/',function(req, res){
 	res.sendFile(__dirname + '/startpage.html');
@@ -23,6 +23,8 @@ app.get('/login.js', function (req, res) {
 app.get('/chat',function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
 
 io.on('connection', function(socket){
   console.log('a user connected');
